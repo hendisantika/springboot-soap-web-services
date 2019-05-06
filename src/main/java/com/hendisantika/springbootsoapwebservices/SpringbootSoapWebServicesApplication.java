ada5e8063da6eca97004b903ccbc5b17e4574f3d
hendisantika <hendisantika@yahoo.co.id>
2019-05-07 02:55:46 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootsoapwebservices/SpringbootSoapWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsoapwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSoapWebServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootsoapwebservices/SpringbootSoapWebServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootsoapwebservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSoapWebServicesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
